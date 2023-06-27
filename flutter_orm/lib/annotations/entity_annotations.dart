8df4d666dbfba98063e200a92322625f8a5e8bf9
saleh <salehyarahmadi066@gmail.com>
2023-06-27 13:48:00 +0330

    documentation

--- flutter_orm/lib/annotations/entity_annotations.dart
+++ flutter_orm/lib/annotations/entity_annotations.dart
@@ -60,6 +60,29 @@ class _EntityFields {
   String get indices => 'indices';
 }
 
+/// Annotation for define primary key for your entity(table).
+/// Defining a field with this annotation is necessary.
+/// Also an entity must have exactly one primary key. In other words,
+/// your entity must have exactly one field with this annotation.
+/// If your primary key variable type be [int], you can set [autoGenerate]
+/// property to true. If [autoGenerate] property is set to true, the field type
+/// must be nullable and don't pass this field in object creation.
+/// ```dart
+/// @Entity()
+/// class Note {
+///   @PrimaryKey(autoGenerate: true)
+///   final int? id;
+///
+///   final String text;
+///   final bool isEdited;
+///
+///   Note({
+///     this.id,
+///     required this.text,
+///     required this.isEdited,
+///   });
+/// }
+/// ```
 class PrimaryKey {
   static _PrimaryKeyFields fields = const _PrimaryKeyFields();
   final String? name;
@@ -79,6 +102,27 @@ class _PrimaryKeyFields {
   String get autoGenerate => 'autoGenerate';
 }
 
+/// This annotation is used for when you want to change default name of
+/// column name of a field in table.
+/// You have to know that the default column name is property name.
+/// ```dart
+/// @Entity()
+/// class Note {
+///   @PrimaryKey(autoGenerate: true)
+///   final int? id;
+///
+///   final String text;
+///
+///   @Column(name: 'edited')
+///   final bool isEdited;
+///
+///   Note({
+///     this.id,
+///     required this.text,
+///     required this.isEdited,
+///   });
+/// }
+/// ```
 class Column {
   static _ColumnFields fields = const _ColumnFields();
   final String name;
@@ -92,10 +136,76 @@ class _ColumnFields {
   String get name => 'name';
 }
 
+/// If you want a field doesn't map to a column in table, you can use
+/// this annotation.
+/// ```dart
+/// @Entity()
+/// class Note {
+///   @PrimaryKey(autoGenerate: true)
+///   final int? id;
+///
+///   final String text;
+///
+///   @Ignore()
+///   final bool isEdited;
+///
+///   Note({
+///     this.id,
+///     required this.text,
+///     required this.isEdited,
+///   });
+/// }
+/// ```
 class Ignore {
   const Ignore();
 }
 
+/// If you want to use an object that you have defined yourself, in your entity,
+/// you can use this annotation. Suppose that the `Note` entity has an
+/// `address` property that is an object itself. In this situation, you can
+/// use this annotation on that field. Embedded objects also can have embedded
+/// fields. Embedded object fields, can have [Column] and [Ignore] annotation,
+/// but they can't have [PrimaryKey].
+/// You should note that the fields of embedded object, merge with
+/// entity fields in table. For example, for the below entity, created table
+/// has these columns: `id`, `text`, `address_lat` and `address_lng`.
+/// If you have noticed, embedded object fields has a prefix in its column
+/// name that is field name by default.
+/// You can change this prefix by change [prefix] property of [Embedded]
+/// annotation.
+///
+/// ```dart
+/// @Entity()
+/// class Note {
+///   @PrimaryKey(autoGenerate: true)
+///   final int? id;
+///
+///   final String text;
+///
+///   @Embedded()
+///   final Address? address;
+///
+///   Note({
+///     this.id,
+///     required this.text,
+///     required this.isEdited,
+///     this.address,
+///   });
+/// }
+///
+/// class Address {
+///   @Column(name: 'lat')
+///   final double latitude;
+///
+///   @Column(name: 'lng')
+///   final double latitude;
+///
+///   Address({
+///     required this.latitude,
+///     required this.latitude,
+///   });
+/// }
+/// ```
 class Embedded {
   static _EmbeddedFields fields = const _EmbeddedFields();
   final String? prefix;
